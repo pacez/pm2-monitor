95f4f38a3b077bb0b59e42098013ec49de6241fc
pace_zhong <pace_zhong@foxmail.com>
2020-08-10 16:37:26 +0800

    UI 优化

--- src/App.js
+++ src/App.js
@@ -1,42 +1,9 @@
-import React, { useState} from 'react';
+import React from 'react';
 import Home from './pages/Home';
 import LoginPage from './pages/Login';
 import { BackTop } from 'antd';
 import { BrowserRouter, Route } from "react-router-dom";
-
 import './App.scss';
-
-// import Request from './util/request';
-
-
-// const App = () => {
-//   const [userInfo, setUserInfo] = useState(false);
-//   const [loading, setLoading] = useState(false);
-
-//   !loading && !userInfo && Request.get('/userInfo', {
-//     beforeSend: () => {
-//       setLoading(true)
-//     } 
-//   }).then(data => {
-//     setUserInfo(data)
-//   }).finally(()=>{
-//     setLoading(false)
-//   });
-
-//   return  <>
-//     {
-//       userInfo && <div div className="App" >
-//         <BrowserRouter>
-//           <Route path="/" exact={true} component={Home} />
-//           <Route path="/login" component={LoginPage} />
-//         </BrowserRouter>
-//         <BackTop
-//           visibilityHeight={50}
-//         />
-//       </div>
-//     }
-//   </>
-// };
 
 const App = () => {
   return <div div className="App" >
